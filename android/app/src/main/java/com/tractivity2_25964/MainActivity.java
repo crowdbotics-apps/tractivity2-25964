89ef74fd144779674ede62cfca1c9463c099fe7e
Crowdbotics <team@crowdbotics.com>
2021-04-27 00:14:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tractivity2_25964/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tractivity2_25964;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tractivity2_25964";
+  }
+}
